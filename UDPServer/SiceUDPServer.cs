4a1fd76cc63afd940f30332238690a515fb923d9
Phillip <phillipwu95@gmail.com>
2020-08-10 00:10:14 +1000

    Working version of Caliburn Micro for server.

--- UDPServer/SiceUDPServer.cs
+++ UDPServer/SiceUDPServer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -7,8 +8,9 @@ using System.Threading.Tasks;
 
 namespace Sice.PoC.UDPServer
 {
-    class SiceUDPServer : IServerInterface
+    public class SiceUDPServer : IServerInterface, IHandle<SiceUDPServer.ServerCommand>
     {
+        private IEventAggregator eventAggregator;
         private UdpClient client;
         CancellationTokenSource source;
         CancellationToken token;
@@ -20,10 +22,12 @@ namespace Sice.PoC.UDPServer
         public ReceivedMessage ReceivedMessage { get; set; }
         public int DataAvailable => client.Available;
 
-        public SiceUDPServer()
+        public SiceUDPServer(IEventAggregator eventAggregator)
         {
             this.ConnectionStatus = false;
             this.currentPort = -1;
+            this.eventAggregator = eventAggregator;
+            eventAggregator.Subscribe(this);
         }
 
         public void Connect()
@@ -32,10 +36,11 @@ namespace Sice.PoC.UDPServer
             {
                 client = new UdpClient(ConnectionPort);
                 currentPort = ConnectionPort;
-                this.ConnectionStatus = true;
                 source = new CancellationTokenSource();
                 token = source.Token;
                 Task.Run(() => Listen(), token);
+                this.ConnectionStatus = true;
+                eventAggregator.PublishOnUIThread(new ServerStatus(true));
             }
         }
 
@@ -57,14 +62,8 @@ namespace Sice.PoC.UDPServer
                 client.Close();
                 this.ConnectionStatus = false;
                 source.Cancel();
+                eventAggregator.PublishOnUIThread(new ServerStatus(false));
             }
-        }
-
-        public event EventHandler<MessageReceivedEventArgs> MessageReceivedEventHandler;
-
-        protected virtual void OnMessageReceived(MessageReceivedEventArgs e)
-        {
-            MessageReceivedEventHandler?.Invoke(this, e);
         }
 
         public async Task GetData()
@@ -73,7 +72,7 @@ namespace Sice.PoC.UDPServer
             var data = await client.ReceiveAsync();
             ReceivedData = Encoding.ASCII.GetString(data.Buffer);
             ReceivedMessage = new ReceivedMessage(DateTime.Now.ToString(), data.RemoteEndPoint.Address.ToString(), ReceivedData);
-            OnMessageReceived(new MessageReceivedEventArgs(ReceivedMessage));
+            this.eventAggregator.PublishOnUIThread(ReceivedMessage);
             using (var db = new ServerContext())
             {
                 db.ReceivedMessages.Add(ReceivedMessage);
@@ -84,6 +83,48 @@ namespace Sice.PoC.UDPServer
         public void Dispose()
         {
             client.Dispose();
+        }
+
+        public void Handle(ServerCommand command)
+        {
+            if(command.Command)
+            {
+                ConnectionIP = command.ConnectionIP;
+                ConnectionPort = command.ConnectionPort;
+                this.Connect();
+            }
+            else
+            {
+                this.Disconnect();
+            }
+        }
+
+        public class ServerCommand
+        {
+            public ServerCommand(bool command, string IP, string port)
+            {
+                this.Command = command;
+                if(IPAddress.TryParse(IP, out IPAddress parseIP))
+                {
+                    ConnectionIP = parseIP;
+                }
+                if(int.TryParse(port, out int parsePort))
+                {
+                    ConnectionPort = parsePort;
+                }
+            }
+            public bool Command { get; set; }
+            public IPAddress ConnectionIP { get; set; }
+            public int ConnectionPort { get; set; }
+        }
+
+        public class ServerStatus
+        {
+            public ServerStatus(bool status)
+            {
+                this.Status = status;
+            }
+            public bool Status { get; set; }
         }
     }
 }
---
+++ UDPServer/ViewModels/ShellViewModel.cs
@@ -0,0 +1,120 @@
+﻿using Caliburn.Micro;
+using Sice.PoC.UDPServer;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Net;
+using System.Windows.Media;
+
+namespace UDPServer.ViewModels
+{
+    public class ShellViewModel : Screen, IHandle<ReceivedMessage>, IHandle<SiceUDPServer.ServerStatus>
+    {
+        private readonly IEventAggregator _eventAggregator;
+        private readonly SiceUDPServer server;
+        public ShellViewModel(IEventAggregator eventAggregator, SiceUDPServer siceUDPServer)
+        {
+            this.IP = ConfigurationManager.AppSettings["IPAddress"];
+            this.Port = ConfigurationManager.AppSettings["PortNumber"];
+            this.server = siceUDPServer;
+            this.ReceivedMessages = new ObservableCollection<string>();
+            this._eventAggregator = eventAggregator;
+            this._eventAggregator.Subscribe(this);
+        }
+
+        private string _iP;
+        private string _port;
+        private string _listeningStatus;
+        private SolidColorBrush _statusTextColour;
+
+        public string IP
+        {
+            get { return _iP; }
+            set {
+                _iP = value;
+                NotifyOfPropertyChange(() => IP);
+            }
+        }
+
+        public string Port
+        {
+            get { return _port; }
+            set
+            {
+                _port = value;
+                NotifyOfPropertyChange(() => Port);
+            }
+        }
+
+        public string ListeningStatus
+        {
+            get { return _listeningStatus; }
+            set
+            {
+                _listeningStatus = value;
+                NotifyOfPropertyChange(() => ListeningStatus);
+            }
+        }
+
+        public SolidColorBrush StatusTextColour
+        {
+            get { return this._statusTextColour; }
+            set
+            {
+                this._statusTextColour = value;
+                NotifyOfPropertyChange(() => StatusTextColour);
+            }
+        }
+
+        public ObservableCollection<string> ReceivedMessages { get; set; }
+        
+        public bool CanConnect(string iP, string port)
+        {
+            if (!IPAddress.TryParse(iP, out IPAddress parseIP))
+            {
+                return false;
+            }
+            if (!int.TryParse(port, out int parsePort))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Connect(string iP, string port)
+        {
+            this.StatusTextColour = new SolidColorBrush(Colors.Green);
+            _eventAggregator.PublishOnBackgroundThread(new SiceUDPServer.ServerCommand(true, IP, Port));
+        }
+
+        public void Disconnect(string iP, string port)
+        {
+            this.StatusTextColour = new SolidColorBrush(Colors.Red);
+            _eventAggregator.PublishOnBackgroundThread(new SiceUDPServer.ServerCommand(false, IP, Port));
+        }
+
+        public void Clear()
+        {
+            this.ReceivedMessages.Clear();
+        }
+
+        public void Handle(ReceivedMessage message)
+        {
+            this.ReceivedMessages.Add(message.Message);
+        }
+
+        public void Handle(SiceUDPServer.ServerStatus status)
+        {
+            if(status.Status)
+            {
+                this.ListeningStatus = "Listening";
+            }
+            else
+            {
+                this.ListeningStatus = "Stopped";
+            }
+        }
+
+        
+    }
+
+}
