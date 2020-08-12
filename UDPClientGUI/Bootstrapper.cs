d68d5e526274acf3f154ab68429bdda4d6074f35
Phillip <phillipwu95@gmail.com>
2020-08-12 19:56:02 +1000

    MVVM implementation for client

---
+++ UDPClientGUI/Bootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using Caliburn.Micro;
+using Sice.PoC.UDPCommGUI;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using UDPCommGUI.ViewModels;
+
+namespace UDPCommGUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private readonly SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+
+        protected override void Configure()
+        {
+            _container.Singleton<IEventAggregator, EventAggregator>();
+            _container.Singleton<IWindowManager, WindowManager>();
+            _container.PerRequest<ShellViewModel>();
+            _container.Singleton<SiceUDPClient>();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
---
+++ UDPClientGUI/Events/ClientStatusChangedEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace UDPCommGUI
+{
+    class ClientStatusChangedEvent
+    {
+        public ClientStatusChangedEvent(bool status)
+        {
+            this.Status = status;
+        }
+
+        public bool Status { get; private set; }
+    }
+}
